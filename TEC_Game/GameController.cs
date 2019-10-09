884a1ba851219ef4ee83d3e8c421a03b14b4600c
EnTaro <apan.kirill@yandex.ru>
2019-10-09 13:10:28 +0300

    2.0

--- TEC_Game/GameController.cs
+++ TEC_Game/GameController.cs
@@ -41,49 +41,77 @@ namespace tec
 
                         //Получаем данные для узлов
                         int node1Id = Int32.Parse(GetSubString(ref line, line.IndexOf(' ')));
+                        string node1IsNew = GetSubString(ref line, 1);
                         int node1X = Int32.Parse(GetSubString(ref line, line.IndexOf(' ')));
                         int node1Y = Int32.Parse(GetSubString(ref line, line.IndexOf(' ')));
 
                         int node2Id = Int32.Parse(GetSubString(ref line, line.IndexOf(' ')));
+                        string node2IsNew = GetSubString(ref line, 1);
                         int node2X = Int32.Parse(GetSubString(ref line, line.IndexOf(' ')));
                         int node2Y = Int32.Parse(GetSubString(ref line, line.IndexOf(' ')));
 
                         //Определеям тип элемента(r - резистор, p - проводимость
                         string type = GetSubString(ref line, 1);
 
-                        //Добавляем в схему узлы
-                        Node node1 = new Node(new Button(), node1Id, node1X, node1Y);
-                        Node node2 = new Node(new Button(), node2Id, node2X, node2Y);
-                        scheme.AddNode(node1);
-                        scheme.AddNode(node2);
+                        Node node1 = scheme.GetNode(node1Id);
+                        Node node2 = scheme.GetNode(node2Id);
+
+                        //Если узлов еще не в схеме, то добавляем их
+                        if (node1 == null)
+                        {
+                            node1 = new Node(new Button(), node1Id, node1X, node1Y);
+                            scheme.AddNode(node1);
+                        }
+
+                        if (node2 == null)
+                        {
+                            node2 = new Node(new Button(), node2Id, node2X, node2Y);
+                            scheme.AddNode(node2);
+                        }
 
                         //Кнопкам узлов добавляем номер
                         node1.GetButton().Content = node1Id.ToString();
                         node2.GetButton().Content = node2Id.ToString();
-                        
+
+                        BaseElement element = null;
+
                         //Добавляем элемент в зависимости от типа
                         if (type == "r")
                         {
-                            var resistor = new Resistor(node1, node2, scheme.GetElementsSize());
-                            scheme.AddElement(resistor);
+                            element = new Resistor(node1, node2, scheme.GetElementsSize());
+                            scheme.AddElement(element);
+                            node1.AddConnectedElement(element);
+                            node2.AddConnectedElement(element);
                         }
                         else
                         {
-                            var conductor = new Conductor(node1, node2, scheme.GetElementsSize());
-                            scheme.AddElement(conductor);
+                            element = new Conductor(node1, node2, scheme.GetElementsSize());
+                            scheme.AddElement(element);
+                            node1.AddConnectedElement(element);
+                            node2.AddConnectedElement(element);
                         }
 
                         //Находим объект игрового окна для добавления элементов в него
-                        Window gameWindow;
+                        Window gameWindow = null;
                         foreach (Window window in Application.Current.Windows)
                             if (window is GameWindow) gameWindow = window;
+
+                        //Добавляем шаблон кнопкам
+                        node1.GetButton().Template = gameWindow.FindResource("NodeTemplate") as ControlTemplate;
+                        node2.GetButton().Template = gameWindow.FindResource("NodeTemplate") as ControlTemplate;
 
                         //формат sh указывает на то, что между двумя нодами подключен только один элемент
                         if (format == "sh")
                         {
-
-
+                            double x1 = X_CENTER - 100 * ((X_SIZE - 1) / 2 - node1X);
+                            double x2 = X_CENTER - 100 * ((X_SIZE - 1) / 2 - node2X);
+                            double y1 = Y_CENTER - 100 * ((Y_SIZE - 1) / 2 - node1Y);
+                            double y2 = Y_CENTER - 100 * ((Y_SIZE - 1) / 2 - node2Y);
+                            node1.GetButton().Margin = new Thickness(x1, y1, 0, 0);
+                            node2.GetButton().Margin = new Thickness(x2, y2, 0, 0);
                             
+
+
                         }
                         //Этот формат показывает, что между 2 нодами есть несколько элементов
                         else if (format == "ff")
